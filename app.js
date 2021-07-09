9301d1032ce5fcc8280949b6e442594c3428bdd7
fkwsurm <guswl543210@gmail.com>
2021-07-09 11:56:14 +0900

    최종

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const app = express();
 const compression = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
+const fs = require('fs');
 const logger = require('morgan')
-//const rateLimit = require("express-rate-limit"); 
-//const limiter = rateLimit({ 
-//  windowMs: 1*60*1000, 
-//  max: 100 
-//  })
+
 const http_server = require('http').createServer(app)
 .listen(8080, () => {
   console.log('server on');
@@ -19,16 +16,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 app.use(helmet());
 app.use(compression());
-app.use(logger({
-  format: 'dev',
-  stream: fs.createWriteStream('app.log', {'flags': 'w'})
-}));
-//app.use(limiter);
+app.use(logger('dev'));
 
- app.get('/user', (res,req) => {
-   try{
-     res.send('유저들입장')
-   } catch(err){
-     console.log(err);
-   }
+ app.get('/user', (req, res) => {
+    res.send('테스트');
  });
